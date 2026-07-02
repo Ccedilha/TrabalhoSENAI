bf641eb7c7903987b5c2e50c0543baded2993647
Ian <ianguilhermepenha@gmail.com>
2026-07-01 21:46:33 -0300

    Primeiro commit.

---
+++ app/script.js
@@ -0,0 +1,6 @@
+function calculoGastos(){
+    let gasto = document.getElementById("gastos").value;
+    let valorGasto = document.getElementById("valorGasto").value;
+
+    let gastoNum = Number(valorGasto)
+}
