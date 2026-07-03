98330edff82aebd197f5ca032a8251660bee46f4
Ian <ianguilhermepenha@gmail.com>
2026-07-02 21:35:39 -0300

    feat: função para calcular o câmbio

--- app/script.js
+++ app/script.js
@@ -1,6 +1,31 @@
-function calculoGastos(){
+const salvarGasto = []
+function calculoGastos() {
     let gasto = document.getElementById("gastos").value;
     let valorGasto = document.getElementById("valorGasto").value;
+    let moeda = document.getElementById("moeda").value;
 
-    let gastoNum = Number(valorGasto)
+    if (moeda === "BRL") {
+        const cambio = 1
+        let resultadoReais = Number(valorGasto) * Number(cambio)
+        console.log(resultadoReais)
+        return;
+    }
+    fetch(`https://economia.awesomeapi.com.br/last/${moeda}-BRL`)
+        .then(function (resposta) {
+            return resposta.json();
+        })
+        .then(function (dados) {
+            if (dados.erro) {
+                alert("...")
+                return;
+            }
+            const chaveMoeda = `${moeda}BRL`
+            let cambio = dados[chaveMoeda]["bid"];
+            let resultadoReais = Number(valorGasto) * Number(cambio)
+            console.log(resultadoReais)
+        })
+}
+
+function adicionaGasto() {
+
 }
